0e0deb3196510c265e51b5f68e6f93ff63a53160
Arthur <32548365+Arthh@users.noreply.github.com>
2022-07-26 07:13:34 -0300

    feat: optimism integrataion

---
+++ src/optimism_mapping.ts
@@ -0,0 +1,50 @@
+import { Organization, User, DelegatorOrganization } from "../generated/schema";
+import {
+  DelegateChanged,
+  DelegateVotesChanged,
+} from "../generated/OptimismToken/OptimismToken";
+import { getDelegateOrganization } from "./shared/getDelegateOrganization";
+import { getFirstTokenDelegatedAt } from "./shared/getFirstTokenDelegatedAt";
+
+export function delegateChanged(event: DelegateChanged): void {
+  let organization = new Organization("optimism");
+  organization.token = "optimism";
+  organization.save();
+
+  let delegate = new User(event.params.toDelegate.toHexString());
+  delegate.save();
+
+  let delegator = new User(event.params.delegator.toHexString());
+  delegator.save();
+
+  let delegatorOrganization = new DelegatorOrganization(
+    `${delegator.id}-${organization.id}`
+  );
+  delegatorOrganization.delegate = delegate.id;
+  delegatorOrganization.delegator = delegator.id;
+  delegatorOrganization.organization = organization.id;
+  delegatorOrganization.save();
+}
+
+export function delegateVotesChanged(event: DelegateVotesChanged): void {
+  let organization = new Organization("optimism");
+  organization.token = "optimism";
+  organization.save();
+
+  let user = new User(event.params.delegate.toHexString());
+  user.save();
+
+  const delegateOrganizationId = `${user.id}-${organization.id}`;
+  const delegateOrganization = getDelegateOrganization(delegateOrganizationId);
+
+  delegateOrganization.delegate = user.id;
+  delegateOrganization.organization = organization.id;
+  delegateOrganization.voteBalance = event.params.newBalance;
+
+  delegateOrganization.firstTokenDelegatedAt = getFirstTokenDelegatedAt(
+    event,
+    delegateOrganization
+  );
+
+  delegateOrganization.save();
+}
